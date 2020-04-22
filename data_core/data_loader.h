55e0f1129e209c2892c71bb21aabc2749cadca1e
MayankB11 <miku.bhushan@gmail.com>
2020-04-22 13:02:23 +0530

    Comments added and documentation generated

--- data_core/data_loader.h
+++ data_core/data_loader.h
@@ -3,19 +3,47 @@
 
 #include "dataset.h"
 
+/*! \class Dataset
+
+This class is used to load the data and store it in the buffer.
+*/
 class DataLoader {
     private:
-        unsigned batch_size_;       // number of data items to be read together
-        unsigned index_;            // current index of the data in the dataset to be read
-        unsigned max_index_;        // maximum number of elements in the dataset
-        Dataset* dataset_;          // pointer to the dataset and its methods
-        float* data_buffer_;        // stores a data item 
-        float* label_buffer_;       // stores the label for the data_buffer_
+        unsigned batch_size_;       /*!< number of data items to be read together. */
+        unsigned index_;            /*!< current index of the data in the dataset to be read. */
+        unsigned max_index_;        /*!< maximum number of elements in the dataset. */
+        Dataset* dataset_;          /*!< pointer to the dataset and its methods. */
+        float* data_buffer_;        /*!< stores a data item . */
+        float* label_buffer_;       /*!< stores the label for the data_buffer_. */
     public:
+
+        /**
+        * Initializes DataLoader class.
+        @param dataset Dataset to initialize to.
+        @param batch_size number of data items to access in a single batch.
+        */    
         DataLoader(Dataset* dataset, unsigned batch_size);      
-        unsigned get_next_batch(float** data, float** labels);  // Returns a batch of data items and their labels in data and labels respectively
-        unsigned getBatchSize();                                // Returns the batch size
-        void reset();                                           // resets the index to the first element in the dataset
+
+        /**
+        * Returns a batch of data items and their labels in data and labels respectively.
+        * @param data stores the data items in it for returning.
+        * @param labels stores the labels in it for returning.
+        */
+        unsigned get_next_batch(float** data, float** labels);
+
+        /**
+        * Returns the batch size  
+        */
+        unsigned getBatchSize();                                
+        
+        /**
+        * resets the index to the first element in the dataset
+        */
+        void reset();                                           
+        
+        /**
+        * Destroy DataLoader
+        */
         ~DataLoader();
 };
 
--- data_core/dataset.h
+++ data_core/dataset.h
@@ -3,18 +3,50 @@
 
 #include <cstdlib>
 
+/*! \class Dataset
+\brief Abstract class Dataset.
+
+This class is inherited by the datasets used (mnist_dataset for now).
+*/
 class Dataset{
     protected:
-        size_t dataset_size_;       // stores the number of elements in dataset
-        size_t input_size_;         // stores the size of a data item without label
-        size_t label_size_;         // stores the size of label
+        size_t dataset_size_;       /*!< stores the number of elements in dataset. */
+        size_t input_size_;         /*!< stores the size of a data item without label. */
+        size_t label_size_;         /*!< stores the size of label. */
     public:
-        virtual size_t getInputDim() = 0;       // returns the size of a data item
-        virtual size_t getLabelDim() = 0;       // returns the size of a label
-        virtual size_t getDatasetSize() = 0;    // returns the number of elements in the dataset
-        virtual void get_item(int index, float* input, float* output) = 0;                   // returns the data item and label present at index in pointers data and label respectively
-        virtual void get_item_range(int start, int end, float* input, float* output) = 0;    // returns the data items and labels from index start variable to index end variable in pointers data_batch and label_batch respectively   
-        virtual void shuffle()=0;               // randomly shuffles the dataset
+
+        /**
+        * returns the size of a data item. 
+        */
+        virtual size_t getInputDim() = 0;       
+
+        /**
+        * returns the size of a label.
+        */
+        virtual size_t getLabelDim() = 0;       
+        
+        /**
+        * returns the number of elements in the dataset. 
+        */
+        virtual size_t getDatasetSize() = 0;    
+        
+        /**
+        * returns the data item and label present at index.
+        * @param index index of the data item and label to fetch 
+        */
+        virtual void get_item(int index, float* input, float* output) = 0;                  
+
+        /**
+        * returns all the data items and labels in the range from index start variable to index end variable.
+        * @param start start index of the range
+        * @paran end end index of the range
+        */ 
+        virtual void get_item_range(int start, int end, float* input, float* output) = 0;    
+        
+        /**
+        * randomly shuffles the dataset.
+        */
+        virtual void shuffle()=0;               
 };
 
 #endif
